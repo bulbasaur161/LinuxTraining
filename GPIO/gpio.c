e5d72bfa5f17c11ead090f3983675f01c2144f17
bulbasaur161 <49242472+bulbasaur161@users.noreply.github.com>
2021-06-25 15:36:53 +0700

    Update gpio.c

--- GPIO/gpio.c
+++ GPIO/gpio.c
@@ -1,6 +1,20 @@
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("bulbasaur161");
+MODULE_AUTHOR("me");
 MODULE_DESCRIPTION("Gpio test");
 MODULE_VERSION("0.1");
+
+static int __init gpio_init(void)
+{
+  pr_info("GPIO_TEST: Initializing the GPIO_TEST LKM\n");
+	return 0;
+}
+
+static void __exit gpio_exit(void)
+{
+  pr_info("GPIO_TEST: End of the LKM!\n");
+}
+
+module_init(gpio_init);
+module_exit(gpio_exit);
