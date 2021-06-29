6d8a31a074e54ccfbf4ab39a535cbb4a10a6ade7
bulbasaur161 <49242472+bulbasaur161@users.noreply.github.com>
2021-06-29 13:21:55 +0700

    Update gpio.c

--- GPIO/gpio.c
+++ GPIO/gpio.c
@@ -100,14 +100,7 @@ static void blink_led1(struct timer_list* timer)
 
 static void blink_led2(unsigned long ptr)
 {
-	if(gpio_get_value(67) != 0)
-	{
-		gpio_set_value (67, 0);
-	}
-	else
-	{
-		gpio_set_value (67, 1);
-	}
+	gpio_set_value (67, ~gpio_get_value(67));
 	
 	// Set timeout
 	led_timer.expires = jiffies + HZ;
