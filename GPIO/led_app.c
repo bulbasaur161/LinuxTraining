4ef60cb5ecb34ec5fd95a8c4ee6a35ce36b8d56a
bulbasaur161 <49242472+bulbasaur161@users.noreply.github.com>
2021-06-29 12:58:06 +0700

    Update led_app.c

--- GPIO/led_app.c
+++ GPIO/led_app.c
@@ -10,9 +10,37 @@ int main(int argc, char *argv[])
 	printf("argc = %d \n", argc);
 	if(argc == 2)
 	{
-		if(strcmp(argv[1], "-g") == 0)
+		if(strcmp(argv[1], "1") == 0)
 		{
+			fd = open(file_name, O_RDWR);
+			if(fd == -1)
+			{
+			    perror("query_apps open\n");
+			    return 2;
+			}
 
+			if(ioctl(fd, 1, 0) == -1)
+			{
+				printf("query_apps ioctl clr\n");
+			}
+
+			close(fd);
+		}
+		else if(strcmp(argv[1], "0") == 0)
+		{
+			fd = open(file_name, O_RDWR);
+			if(fd == -1)
+			{
+			    perror("query_apps open\n");
+			    return 2;
+			}
+
+			if(ioctl(fd, 0, 0) == -1)
+			{
+				printf("query_apps ioctl clr\n");
+			}
+
+			close(fd);
 		}
 	}
 	else
@@ -20,18 +48,5 @@ int main(int argc, char *argv[])
 		printf("input app 1 or app 0\n");
 	}
 
-	fd = open(file_name, O_RDWR);
-	if(fd == -1)
-	{
-	    perror("query_apps open\n");
-	    return 2;
-	}
-	
-	if(ioctl(fd, 1, 0) == -1)
-	{
-		printf("query_apps ioctl clr\n");
-	}
-
-	close(fd);
 	return 0;
 }
