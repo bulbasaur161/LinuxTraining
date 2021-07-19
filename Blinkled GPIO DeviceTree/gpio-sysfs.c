8b7c784fa09e87fecd24fbbb916bb2c289a78205
bulbasaur161 <49242472+bulbasaur161@users.noreply.github.com>
2021-07-19 17:12:07 +0700

    Create gpio-sysfs.c

---
+++ Blinkled GPIO DeviceTree/gpio-sysfs.c
@@ -0,0 +1,153 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/io.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include<linux/gpio/consumer.h>
+
+#undef pr_fmt
+#define pr_fmt(fmt) "%s : " fmt,__func__
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("--");
+MODULE_DESCRIPTION("A gpio sysfs driver");
+
+/*Device private data structure */
+struct gpiodev_private_data
+{
+	char label[20];
+	struct gpio_desc *desc;
+};
+
+/*Driver private data structure */
+struct gpiodrv_private_data
+{
+	int total_devices;
+	struct class *class_gpio;
+	struct device **dev;
+};
+
+struct gpiodrv_private_data gpio_drv_data;
+
+int gpio_sysfs_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	int i = 0;
+	int ret;
+	const char *name;
+
+	/*parent device node */
+	struct device_node *parent = pdev->dev.of_node;
+	struct device_node *child = NULL;
+	struct gpiodev_private_data *dev_data;
+
+  //Get number child node of bone_gpio_devs parent node in device tree
+	gpio_drv_data.total_devices = of_get_child_count(parent);
+	if(!gpio_drv_data.total_devices){
+		dev_err(dev,"No devices found\n");
+		return -EINVAL;
+	}
+
+	dev_info(dev,"Total devices found = %d\n",gpio_drv_data.total_devices);
+
+  //Allocate devices
+	gpio_drv_data.dev = devm_kzalloc(dev, sizeof(struct device *) * gpio_drv_data.total_devices , GFP_KERNEL);
+
+	for_each_available_child_of_node(parent,child)
+	{
+
+		dev_data = devm_kzalloc(dev,sizeof(*dev_data), GFP_KERNEL);
+		if(!dev_data){
+			dev_err(dev,"Cannot allocate memory\n");
+			return -ENOMEM;
+		}
+
+    //Get data from device tree, property "label"
+		if(of_property_read_string(child,"label",&name) )
+		{
+			dev_warn(dev,"Missing label information\n");
+			snprintf(dev_data->label,sizeof(dev_data->label),"unkngpio%d",i);
+		}else{
+      //Copy data to gpio private data
+			strcpy(dev_data->label,name);
+			dev_info(dev,"GPIO label = %s\n",dev_data->label);
+			
+		}
+
+		dev_data->desc = devm_fwnode_get_gpiod_from_child(dev,"bone",&child->fwnode,\
+							GPIOD_ASIS,dev_data->label);
+		if(IS_ERR( dev_data->desc)){
+			ret = PTR_ERR(dev_data->desc);
+			if(ret == -ENOENT)
+				dev_err(dev,"No GPIO has been assigned to the requested function and/or index\n");
+			return ret;
+		}
+
+		/* set the gpio direction to output */
+		ret = gpiod_direction_output(dev_data->desc,0);
+		if(ret){
+			dev_err(dev,"gpio direction set failed \n");
+			return ret;
+		}
+
+		/*Create devices under /sys/class/bone_gpios */
+		gpio_drv_data.dev[i] = device_create_with_groups(gpio_drv_data.class_gpio,dev,0,dev_data,gpio_attr_groups,\
+								dev_data->label);
+		if(IS_ERR(gpio_drv_data.dev[i])){
+			dev_err(dev,"Error in device_create \n");
+			return PTR_ERR(gpio_drv_data.dev[i]);
+		}
+
+		i++;
+	}
+	return 0;
+}
+
+struct of_device_id  gpio_device_match[] = 
+{
+	{.compatible = "org,bone-gpio-sysfs"},
+	{ }
+};
+
+struct platform_driver gpiosysfs_platform_driver = 
+{
+	.probe = gpio_sysfs_probe,
+	.remove = gpio_sysfs_remove,
+	.driver = {
+	       .name = "bone-gpio-syfs",                          // in case not use device tree
+	       .of_match_table = of_match_ptr(gpio_device_match)  // in case use device tree
+	}      
+
+};
+
+int __init gpio_sysfs_init(void)
+{
+  // Create class "/sys/class/bone_gpios"
+	gpio_drv_data.class_gpio = class_create(THIS_MODULE,"bone_gpios");
+	if(IS_ERR(gpio_drv_data.class_gpio)){
+		pr_err("Error in creating class \n");
+		return PTR_ERR(gpio_drv_data.class_gpio);
+	}
+
+  // Register driver
+	platform_driver_register(&gpiosysfs_platform_driver);
+	pr_info("module load success\n");
+	return 0;
+}
+
+
+void __exit gpio_sysfs_exit(void)
+{
+	platform_driver_unregister(&gpiosysfs_platform_driver);
+	
+	class_destroy(gpio_drv_data.class_gpio);
+}
+
+module_init(gpio_sysfs_init);
+module_exit(gpio_sysfs_exit);
