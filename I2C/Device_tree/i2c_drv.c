1b07e642fd0bf19cbb4d00040eb9b175f707d798
bulbasaur161 <49242472+bulbasaur161@users.noreply.github.com>
2021-07-27 11:47:16 +0700

    Update i2c_drv.c

--- I2C/Device_tree/i2c_drv.c
+++ I2C/Device_tree/i2c_drv.c
@@ -24,7 +24,7 @@ static int total_device = 0;
 /*Device private data structure */
 struct i2c_device_data {
 	struct i2c_client *client;
-	dev_t dev; // For major and minor number
+	dev_t dev_num; // For major and minor number
 	u8 *buf;
 	u16 value;
 	struct cdev cdev;
@@ -39,19 +39,87 @@ struct i2c_driver_data
 	struct device *device;
 };
 
+static ssize_t my_read(struct file *f, char *buf, size_t count, loff_t *off)
+{
+	pr_info("Read device file\n");
+	return 0;
+}
+
+static ssize_t my_write(struct file *f, const char *buf, size_t count, loff_t *off)
+{
+	pr_info("Write to device file\n");
+	return 0;
+}
+
+
+static int my_open (struct inode *i, struct file *f)
+{
+	pr_info("Open device file\n");
+	return 0;
+}
+
+struct file_operations fops = {
+	.open		= my_open,
+	.release 	= my_close,
+	.read		= my_read,
+	.write		= my_write,
+};
+
 /*Driver's private data */
 struct i2c_driver_data ds3231_driver_data;
 
 /* Probe Function to invoke the I2c Driver */
 static int ds3231_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
+	int ret;
 	struct i2c_device_data *data;
+	struct device *dev = &client->dev;
 	
 	// Allocate i2c private data
 	data = devm_kzalloc(&client->dev, sizeof(struct i2c_device_data), GFP_KERNEL);
 	// Assign pointer to private data, it same dev_set_drvdata () func
 	i2c_set_clientdata(client, data);
 
+	if(device_create(data->class, NULL, data->dev, NULL, "i2c_drv%d",0) == NULL)
+	{
+		printk(KERN_ALERT"Unable to create the device...\n");
+		class_destroy(data->class);
+		unregister_chrdev_region(data->dev, 1);
+		return -1;
+	}
+	
+	/*4. Get the device number */
+	data->dev_num = ds3231_driver_data.device_num_base + total_device;
+	
+	// Register entry point
+	// Init cdev struct
+	cdev_init(&data->cdev,&fops);
+	data->cdev.owner = THIS_MODULE;
+	// Register cdev struct to kernel
+	// param1: &data->cdev - cdev struct
+	// param2: data->dev_num - first device number of list device will be link with this cdev
+	// param3: number of device will be link with this cdev
+	ret = cdev_add(&data->cdev,data->dev_num,1);
+	if(ret < 0){
+		dev_err(dev,"Cdev add failed\n");
+		return ret;
+	}
+	
+	/*6. Create device file for the detected platform device */
+	// param1: ds3231_driver_data.class - class create in /sys/class by class_create func
+	// param2: dev = parrent device
+	// param3: data->dev_num - device number of device
+	// param4: NULL - additon data
+	// param5: "i2cdev-%d" - device name, will be created in /dev
+	ds3231_driver_data.device = device_create(ds3231_driver_data.class, dev, data->dev_num,NULL,\
+								"i2cdev-%d",total_devices);
+	if(IS_ERR(ds3231_driver_data.device)){
+		dev_err(dev,"Device create failed\n");
+		ret = PTR_ERR(ds3231_driver_data.device);
+		cdev_del(&data->cdev);
+		return ret;
+	}
+	
 	pr_info("module%d load success\n", total_device);
 	total_device ++;
 	return 0;
@@ -97,6 +165,7 @@ static struct i2c_driver ds3231_I2C_drv = {
 static int __init i2c_client_drv_init(void)
 {
 	int ret;
+	pr_info("Init driver\n");
 
 	/*1. Dynamically allocate a device number for MAX_DEVICES */
 	// Allocate device number: minor of first device 0, number of device allocated MAX_DEVICES
@@ -128,6 +197,7 @@ module_init(i2c_client_drv_init);
 /* Exit module */
 static void __exit i2c_client_drv_exit(void)
 {
+	pr_info("Remove driver\n");
 	i2c_del_driver(&ds3231_I2C_drv);
 	class_destroy(ds3231_driver_data.class);
 	unregister_chrdev_region(ds3231_driver_data.device_num_base,MAX_DEVICES);
