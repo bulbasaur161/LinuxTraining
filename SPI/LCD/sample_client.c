fba41aa1a588b51cc5ef3d34c85dda6014aca1e3
bulbasaur161 <49242472+bulbasaur161@users.noreply.github.com>
2021-09-15 17:47:24 +0700

    Update sample_client.c

--- SPI/LCD/sample_client.c
+++ SPI/LCD/sample_client.c
@@ -122,11 +122,13 @@ static int sample_probe(struct spi_device *spi)
 		return -1;
 	}
 	
-	res =  spi_write(spi, &buf, sizeof(buf));
+	res = spi_write_then_read(spi, &buf, sizeof(buf), &recv, sizeof(recv))
+	
+	//res =  spi_write(spi, &buf, sizeof(buf));
 	printk(KERN_INFO "Write Result %d value: %u %u %u %u %u\n", res, buf[0], buf[1], buf[2], buf[3], buf[4]);
 	/* spi_read to read the data form our spi */
 	mdelay(2);
-	res = spi_read(spi, &recv, sizeof(recv));
+	//res = spi_read(spi, &recv, sizeof(recv));
 	printk(KERN_INFO "Got Result %d value: %u %u %u %u %u\n", res, recv[0], recv[1], recv[2], recv[3], recv[4]);
 	
 	return 0;
