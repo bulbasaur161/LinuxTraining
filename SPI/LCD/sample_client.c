a7cef0d09852b1f7a1eef095d850b0eb9a23c576
bulbasaur161 <49242472+bulbasaur161@users.noreply.github.com>
2021-09-18 14:21:23 +0700

    Update sample_client.c

--- SPI/LCD/sample_client.c
+++ SPI/LCD/sample_client.c
@@ -38,89 +38,37 @@ struct sample_data {
 	struct class *class;
 };
 
-
-static ssize_t sample_read(struct file* f, char *buf, size_t count, loff_t *f_pos)
-{
-	int res;
-	struct sample_data *dev = (struct sample_data*) (f->private_data);
-	
-	struct spi_transfer tr = 
-    	{
-		.tx_buf	= &txbuf,
-		.rx_buf = &rxbuf,
-		.len = 1,
-	};
-	res = spi_sync_transfer(dev->spi, &tr, 1);
-	printk(KERN_INFO "spi_sync_transfer Write Result %d value: %u %u %u %u %u\n", res, txbuf[0], txbuf[1], txbuf[2], txbuf[3], txbuf[4]);
-	printk(KERN_INFO "spi_sync_transfer Got Result %d value: %u %u %u %u %u\n", res, rxbuf[0], rxbuf[1], rxbuf[2], rxbuf[3], rxbuf[4]);
-	rxbuf[0] = 0;
-	
-	mdelay(2);
-	
-	res =  spi_write(dev->spi, &txbuf, sizeof(txbuf[0]));
-	printk(KERN_INFO "spi_write Write Result %d value: %u %u %u %u %u\n", res, txbuf[0], txbuf[1], txbuf[2], txbuf[3], txbuf[4]);
-	/* spi_read to read the data form our spi */
-	mdelay(2);
-	res = spi_read(dev->spi, &rxbuf2, sizeof(rxbuf2[0]));
-	printk(KERN_INFO "Got Result %d value: %u %u %u %u %u\n", res, rxbuf2[0], rxbuf2[1], rxbuf2[2], rxbuf2[3], rxbuf2[4]);
-	rxbuf2[0] = 0;
-	
-	return 0;
-}
-
-static int sample_close(struct inode *i, struct file *file)
-{
-	return 0;
-}
-
-static int sample_open(struct inode *i, struct file *f)
-{
-	struct sample_data *data = container_of(i->i_cdev, struct sample_data, cdev);
-	if(data == NULL)
-	{
-		printk("data is null\n");
-		return -1;
-	}
-	f->private_data = data;
-
-	return 0;
-}
-
-struct file_operations fops = {
-	.open = sample_open,
-	.release = sample_close,
-	.read = sample_read,
-};
-
 void spiWrite_command(void *dev, unsigned char c)
 {
-	int res;
-	struct spi_transfer tr = 
-    	{
-		.tx_buf	= &c,
-		.rx_buf = &rxbuf,
-		.len = 1,
-	};
-	
 	gpio_set_value(LCD_DC_PIN, Low);
 	spi_write(dev, &c, sizeof(c));
+	
+	//int res;
+	//struct spi_transfer tr = 
+    	//{
+	//	.tx_buf	= &c,
+	//	.rx_buf = &rxbuf,
+	//	.len = 1,
+	//};
 	//res = spi_sync_transfer(dev, &tr, 1);
+	//res = spi_write_then_read(dev, &str, 1, &recv, 1);
 	//printk(KERN_INFO "spi_sync_transfer Got Result %d value: %u %u %u %u %u\n", res, rxbuf[0], rxbuf[1], rxbuf[2], rxbuf[3], rxbuf[4]);
 }
 
 void spiWrite_data(void *dev, unsigned char c)
 {
-	int res;
-	struct spi_transfer tr = 
-    	{
-		.tx_buf	= &c,
-		.rx_buf = &rxbuf,
-		.len = 1,
-	};
-	
 	gpio_set_value(LCD_DC_PIN, High);
 	spi_write(dev, &c, sizeof(c));
+	
+	//int res;
+	//struct spi_transfer tr = 
+    	//{
+	//	.tx_buf	= &c,
+	//	.rx_buf = &rxbuf,
+	//	.len = 1,
+	//};
 	//res = spi_sync_transfer(dev, &tr, 1);
+	//res = spi_write_then_read(dev, &str, 1, &recv, 1);
 	//printk(KERN_INFO "spi_sync_transfer Got Result %d value: %u %u %u %u %u\n", res, rxbuf[0], rxbuf[1], rxbuf[2], rxbuf[3], rxbuf[4]);
 }
 
@@ -201,79 +149,8 @@ void LCD_Clear(void *dev, unsigned int j)
 	gpio_set_value(LCD_CS_PIN, High);  //CS   
 }
 
-static int sample_probe(struct spi_device *spi)
+void lcd_init(void *spi)
 {
-	struct sample_data *data;
-	int init_result;
-	int res, ret;
-	int i = 0;
-	
-	ENTER();
-
-	data = devm_kzalloc(&spi->dev, sizeof(struct sample_data), GFP_KERNEL);
-	data->spi = spi;
-	spi_set_drvdata(spi,data);
-
-	init_result = alloc_chrdev_region(&data->devt, 0, 1, "spi_smp");
-
-	if (0 > init_result)
-	{
-		printk(KERN_ALERT "Device Registration failed\n");
-		unregister_chrdev_region(data->devt, 1);
-		return -1;
-	}
-	printk("Major Nr: %d\n", MAJOR(data->devt));
-	
-	cdev_init(&data->cdev, &fops);
-
-	// Register the file ops
-	if (cdev_add(&data->cdev, data->devt, 1) == -1)
-	{
-		printk( KERN_ALERT "Device addition failed\n" );
-		device_destroy(data->class, data->devt);
-		class_destroy(data->class);
-		unregister_chrdev_region(data->devt, 1 );
-		return -1;
-	}
-
-	if ((data->class = class_create(THIS_MODULE, "spisample")) == NULL)
-	{
-		printk( KERN_ALERT "Class creation failed\n" );
-		unregister_chrdev_region(data->devt, 1);
-		return -1;
-	}
-	 //Create the device file with name spi_smp0
-	if (device_create(data->class, NULL, data->devt, NULL, "spi_smp%d", 0) == NULL)
-	{
-		printk( KERN_ALERT "Device creation failed\n" );
-		class_destroy(data->class);
-		unregister_chrdev_region(data->devt, 1);
-		return -1;
-	}
-	
-	// Check invalid gpio
-	for (i = 0; i< PIN_NUMBER; i++)
-	{
-		if (!gpio_is_valid(gpioButton[i]))
-		{
-			pr_info("This gpio pin %d is not valid\n", gpioButton[i]);
-			return -1;
-		}
-	}
-	
-	// Request
-	for (i = 0; i< PIN_NUMBER; i++)
-	{
-		ret = gpio_request(gpioButton[i], "lcd");
-		if (ret < 0)
-		{
-			pr_info("Failed to request GPIO\n");
-			return -1;
-		}
-		//config pin is output
-		gpio_direction_output(gpioButton[i], 0);
-	}
-	
 	//LCD reset
 	gpio_set_value(LCD_RESET_PIN, High);
 	mdelay(5);
@@ -349,28 +226,120 @@ static int sample_probe(struct spi_device *spi)
 	spiWrite_command(spi, 0x2c);
 	
 	gpio_set_value(LCD_CS_PIN, High);  //CS
+}
+
+static ssize_t sample_read(struct file* f, char *buf, size_t count, loff_t *f_pos)
+{
+	int res;
+	struct sample_data *dev = (struct sample_data*) (f->private_data);
 	
-	Rect(spi, 50, 100, 150, 200,50000); // rectangle at x, y, with, hight, color
-	LCD_Clear(spi, 0xf800);
-	LCD_Clear(spi, 0x07E0);
-	LCD_Clear(spi, 0x001F);
-	LCD_Clear(spi, 0x0); 
+	Rect(dev->spi, 50, 100, 150, 200,50000); // rectangle at x, y, with, hight, color
+	LCD_Clear(dev->spi, 0xf800);
+	LCD_Clear(dev->spi, 0x07E0);
+	LCD_Clear(dev->spi, 0x001F);
+	LCD_Clear(dev->spi, 0x0);
 	
-	//struct spi_transfer tr = 
-    	//{
-	//	.tx_buf	= &txbuf,
-	//	.rx_buf = &rxbuf,
-	//	.len = 1,
-	//};
-	//spi_sync_transfer(spi, &tr, 1);
-	//res = spi_write_then_read(spi, &buf, sizeof(buf), &recv, sizeof(recv));
-	//res =  spi_write(spi, &buf, sizeof(buf));
-	//printk(KERN_INFO "Write Result %d value: %u %u %u %u %u\n", res, txbuf[0], txbuf[1], txbuf[2], txbuf[3], txbuf[4]);
-	/* spi_read to read the data form our spi */
-	//mdelay(2);
-	//res = spi_read(spi, &recv, sizeof(recv));
-	//printk(KERN_INFO "Got Result %d value: %u %u %u %u %u\n", res, rxbuf[0], rxbuf[1], rxbuf[2], rxbuf[3], rxbuf[4]);
-	//rxbuf[0] = 0;
+	return 0;
+}
+
+static int sample_close(struct inode *i, struct file *file)
+{
+	return 0;
+}
+
+static int sample_open(struct inode *i, struct file *f)
+{
+	struct sample_data *data = container_of(i->i_cdev, struct sample_data, cdev);
+	if(data == NULL)
+	{
+		printk("data is null\n");
+		return -1;
+	}
+	f->private_data = data;
+
+	return 0;
+}
+
+struct file_operations fops = {
+	.open = sample_open,
+	.release = sample_close,
+	.read = sample_read,
+};
+
+static int sample_probe(struct spi_device *spi)
+{
+	struct sample_data *data;
+	int init_result;
+	int res, ret;
+	int i = 0;
+	
+	ENTER();
+
+	data = devm_kzalloc(&spi->dev, sizeof(struct sample_data), GFP_KERNEL);
+	data->spi = spi;
+	spi_set_drvdata(spi,data);
+
+	init_result = alloc_chrdev_region(&data->devt, 0, 1, "spi_smp");
+
+	if (0 > init_result)
+	{
+		printk(KERN_ALERT "Device Registration failed\n");
+		unregister_chrdev_region(data->devt, 1);
+		return -1;
+	}
+	printk("Major Nr: %d\n", MAJOR(data->devt));
+	
+	cdev_init(&data->cdev, &fops);
+
+	// Register the file ops
+	if (cdev_add(&data->cdev, data->devt, 1) == -1)
+	{
+		printk( KERN_ALERT "Device addition failed\n" );
+		device_destroy(data->class, data->devt);
+		class_destroy(data->class);
+		unregister_chrdev_region(data->devt, 1 );
+		return -1;
+	}
+
+	if ((data->class = class_create(THIS_MODULE, "spisample")) == NULL)
+	{
+		printk( KERN_ALERT "Class creation failed\n" );
+		unregister_chrdev_region(data->devt, 1);
+		return -1;
+	}
+	 //Create the device file with name spi_smp0
+	if (device_create(data->class, NULL, data->devt, NULL, "spi_smp%d", 0) == NULL)
+	{
+		printk( KERN_ALERT "Device creation failed\n" );
+		class_destroy(data->class);
+		unregister_chrdev_region(data->devt, 1);
+		return -1;
+	}
+	
+	// Check invalid gpio
+	for (i = 0; i< PIN_NUMBER; i++)
+	{
+		if (!gpio_is_valid(gpioButton[i]))
+		{
+			pr_info("This gpio pin %d is not valid\n", gpioButton[i]);
+			return -1;
+		}
+	}
+	
+	// Request
+	for (i = 0; i< PIN_NUMBER; i++)
+	{
+		ret = gpio_request(gpioButton[i], "lcd");
+		if (ret < 0)
+		{
+			pr_info("Failed to request GPIO\n");
+			return -1;
+		}
+		//config pin is output
+		gpio_direction_output(gpioButton[i], 0);
+	}
+	
+	lcd_init(spi);
 	
 	return 0;
 }
